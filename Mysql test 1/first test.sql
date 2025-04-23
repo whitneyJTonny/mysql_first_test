26161d68a24044e1d860f2a55f26a0681bb289d2
whitneyJTonny <whitneyjosephin042@gmail.com>
2025-04-23 12:16:32 +0300

    Update first test.sql

--- Mysql test 1/first test.sql
+++ Mysql test 1/first test.sql
@@ -1,32 +1,37 @@
 
    CREATE TABLE Finance (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    tution_fee INT,
-    administration_fee INT,
-    exams_fee INT,
-    medical_fee INT
+    tution_fee DECIMAL(10,2),
+    administration_fee DECIMAL(10,2),
+    exams_fee DECIMAL(10,2),
+    medical_fee DECIMAL(10,2)
 );   
   
-  -- USE tblstudent_db;
-  -- SELECT *FROM student
-  -- WHERE Y1S1
+   SELECT  studentName, StudentID, Tutuion_Fee
+   (SELECT Tuition_Fee FROM tblFinance WHERE tblFinance.StudentID = tblStudent.StudentID) AS Tuition_Fee
+   FROM tblStudent
+   WHERE Year = Y1S1;
+
   
-  -- USE tblstudent_db;
- -- SELECT * FROM Finance
- -- WHERE Tuition_Fee >= 30000
+   SELECT studentName, StudentID, Tutuion_Fee
+   FROM Finance
+   WHERE Tuition_Fee >= 30000
   
---  USE tblstudent_db;
---  INSERT INTO tblstudent(StudentName, Birthdate, Year, MobileNo)
---  VALUES (Dan, 20/4/2001, Y1S1, 0723456123)
+
+  INSERT INTO tblstudent(StudentName, Birthdate, Year, MobileNo)
+  VALUES (Dan, 20/4/2001, Y1S1, 0723456123)
  
---  USE tblstudent_db;
---  UPDATE tblstudent
---  SET StudentName = Nelson
---  WHERE StudentID = 26
+  
+  UPDATE tblstudent
+  SET StudentName = Nelson
+  WHERE StudentID = 26
  
- USE tblstudent_db;
+ 
  SELECT MAX(Tuition_Fee) AS HighestAmount FROM Finance;
  SELECT MIN(Tuition_Fee) AS LowestAmount FROM Finance;
+ FROM tblFinance;
+
+
  
  
  
